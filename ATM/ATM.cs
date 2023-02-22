b06207282564ee783c4ff29016ca4503551b8e67
Serhii <mywaifuqueen@gmail.com>
2023-02-22 17:50:53 +0000

    Add project files.

---
+++ ATM/ATM.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATM
+{
+    class ATM
+    {
+        private double summ;
+        private ICheck check;
+        private ICurrency currency;
+        
+        public ATM()
+        {
+            currency = new Hryvna();
+            summ= 0;
+            check = null;
+        }
+        public void Init()
+        {
+            Console.WriteLine("Enter summ:");
+            summ = Convert.ToDouble(Console.ReadLine());
+        }
+        public void TopUp(double summ)
+        {
+            if (summ < 0) throw new Exception("Top up summ can't be less than 0");
+            if (check != null)
+                check.Check(this.summ,summ, true);
+            this.summ += summ;
+        }
+        public void Decrease(double summ) {
+            if (summ < 0) throw new Exception("Decrease summ can't be less than 0");
+            if (check != null)
+                check.Check(this.summ, summ, false);
+            this.summ -= summ;
+        }
+
+        public void SetCheck(ICheck pICheck)
+        {
+            check= pICheck;
+        }
+        public void SetCurrency(ICurrency pICurrency)
+        {
+           currency = pICurrency;
+           summ = currency.Convert(summ);
+        }
+        public void PrintBalance()
+        {
+            currency.CheckBalance(this.summ);
+        }
+    }  
+}
+
+    interface ICheck
+    {
+        void Check(double current_summ,double summm, bool type);
+    }
+    class DigitalCheck : ICheck { 
+    
+        public void Check(double current_summ, double summ, bool type) {
+            Console.WriteLine("------Digital check------");
+            Console.WriteLine(DateTime.Now.ToString());
+            Console.WriteLine("Prev. Summ - " + current_summ);
+            if (type == true)
+            {
+                Console.WriteLine("Topped up! + " + summ);
+                Console.WriteLine("Total: " + (current_summ + summ));
+            }
+            else
+            {
+                Console.WriteLine("Withdrawn - " + summ);
+                Console.WriteLine("Total: " + (current_summ - summ));
+            }
+            Console.WriteLine("-----------------------------");
+    }      
+    }   
+    class NonDigitalCheck : ICheck
+    {
+    public void Check(double current_summ, double summ, bool type)
+    {
+        Console.WriteLine("-----'Non Digital check'-----");
+        Console.WriteLine(DateTime.Now.ToString());
+        Console.WriteLine("Prev. Summ - " + current_summ);
+        if (type == true)
+        {
+            Console.WriteLine("Topped up! + " + summ);
+            Console.WriteLine("Total: " + (current_summ + summ));
+        }
+        else
+        {
+            Console.WriteLine("Withdrawn - " + summ);
+            Console.WriteLine("Total: " + (current_summ - summ));
+        }
+        Console.WriteLine("-----------------------------");
+    }
+}
+
+    interface ICurrency
+    {
+        void CheckBalance(double summ);
+        double Convert(double summ);
+    }
+
+    class Hryvna:ICurrency
+    {
+        public void CheckBalance(double summ)
+        {
+        Console.WriteLine("Your current balance is " + Math.Round(summ,2) + " hryvnas!");
+        }
+        public double Convert(double summ)
+        {
+            return summ * 36.5686;
+        }
+}
+    class Dollar:ICurrency
+    {
+        public void CheckBalance(double summ)
+        {
+            Console.WriteLine("Your current balance is " + Math.Round(summ, 2) + " dollars!");
+        }
+        public double Convert(double summ)
+        {
+            return summ * 0.0273;
+        }
+}
+
---
+++ ATM/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ATM ATM= new ATM();
+            ATM.SetCheck(new DigitalCheck());
+            ATM.SetCurrency(new Hryvna());
+
+            short choice = -1;
+            int currency = 0;
+            while (choice != 0)
+            {
+                Console.Clear();
+                ATM.PrintBalance();
+
+                Console.WriteLine("0 - Exit\n1 - Top up\n2 - Withdraw\n3 - Change Currency\n4 - Change Check Properties");
+                choice = Convert.ToInt16(Console.ReadLine());
+                Console.Clear();
+                switch (choice)
+                {
+                    case 0:
+                        return;
+                    case 1:
+                        Console.WriteLine("Enter summ to top up:");
+                        ATM.TopUp(Convert.ToDouble(Console.ReadLine()));
+                        break;
+                    case 2:
+                        Console.WriteLine("Enter summ to withdraw:");
+                        ATM.Decrease(Convert.ToDouble(Console.ReadLine()));
+                        break;
+                    case 3:
+                        if(currency % 2 == 0)
+                        {
+                            ATM.SetCurrency(new Dollar());
+                        }
+                        else
+                            ATM.SetCurrency(new Hryvna());
+                        currency++;
+                        Console.WriteLine("Success!");
+                        break;
+                    case 4:
+                        Console.WriteLine("1 - No check\n2 - Digital Check\n3 - Non Digital Check");
+                        choice = Convert.ToInt16(Console.ReadLine());
+                        switch (choice)
+                        {
+                        case 1:
+                            ATM.SetCheck(null);
+                            break;
+                        case 2:
+                            ATM.SetCheck(new DigitalCheck());
+                            break;
+                        case 3:
+                            ATM.SetCheck(new NonDigitalCheck());
+                            break;
+                            default:
+                                break;
+                        }
+                        Console.WriteLine("Success!");
+                        break;
+                }
+                Console.ReadLine();
+            }
+
+
+
+        }
+    }
+}
